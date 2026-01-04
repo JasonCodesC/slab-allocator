833a21e7aee527406cf842980e81654d05be2043
Jason <150879336+JasonCodesC@users.noreply.github.com>
2026-01-04 02:20:07 -0500

    moving

--- src/page_pool.cpp
+++ src/page_pool.cpp
@@ -1,6 +1,12 @@
 #include "../include/page_pool.h"
 #include <sys/mman.h>
 
+PagePool::~PagePool() noexcept {
+    for (std::size_t size_class = 0; size_class < NumClasses; ++size_class) {
+        for (void* page : pages[size_class]) { free_page(page, page_size); }
+    }
+}
+
 void* __attribute__((noinline)) PagePool::alloc_page(std::size_t bytes) noexcept {
     void* p = ::mmap(nullptr, bytes, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
     return (p == MAP_FAILED) ? nullptr : p;
